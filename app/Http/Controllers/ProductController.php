a42e2b1f694f86492f3c278602f36d4c27836033
kiervz <kierveymaghanoy24@gmail.com>
2020-07-08 09:41:26 +0800

    ADDED: Product resource transformer

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductResource;
+use App\Model\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function index()
+    {
+        return Product::all();
+    }
 
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
 }
