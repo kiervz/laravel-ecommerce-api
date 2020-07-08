06825342fe6bf7638cecf361e608d981cc0f2432
kiervz <kierveymaghanoy24@gmail.com>
2020-07-08 15:22:02 +0800

    ADDED: Create Product and Product request

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
@@ -9,6 +10,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except(['index', 'show']);
+    }
     public function index()
     {
         return ProductCollection::collection(Product::paginate(10));
@@ -17,5 +22,22 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         return new ProductResource($product);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response()->json([
+            'data' => new ProductResource($product),
+        ]);
+
+        return $request->all();
     }
 }
