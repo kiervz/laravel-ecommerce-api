2df371101af218c512f26145b98c779700872299
kiervz <kierveymaghanoy24@gmail.com>
2020-07-09 16:28:45 +0800

    ADDED: Create Review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Model\Review;
 use App\Model\Product;
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\Review\ReviewResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
+
     public function index(Product $product)
     {
         return ReviewResource::collection($product->reviews);
+    }
+
+    public function store(ReviewRequest $request, Product $product)
+    {
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+
+        return response()->json([
+            'data' => new ReviewResource($review)
+        ], Response::HTTP_CREATED);
     }
 
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $guarded = [];
+
     public function products()
     {
         return $this->belongsTo(Product::class);
